0184f743e7f748379d737a5a2fcb89e68dabd347
Fernando <00195316@uca.edu.sv>
2019-04-05 09:00:30 -0600

    Trabajo Final

--- app/src/main/java/com/example/laboratorio_04_00195316/activities/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio_04_00195316/activities/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v7.widget.LinearLayoutManager
 import com.example.laboratorio_04_00195316.R
+import com.example.laboratorio_04_00195316.adapters.MovieAdapter
 import com.example.laboratorio_04_00195316.model.Movie
 import com.example.laboratorio_04_00195316.utils.NetworkUtils
 import com.google.gson.Gson
@@ -14,9 +16,27 @@ import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
+    private var movieList: ArrayList<Movie> = ArrayList()
+    private lateinit var movieAdapter: MovieAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun initRecylerView(){
+        var viewManager = LinearLayoutManager(this)
+
+    }
+
+    fun initSearchbar() {
+        add_movie_btn.setOnClickListener{if (!movie_name_et.text.isEmpty()){ FetchMovie().execute(movie_name_et.toString()) }}
+    }
+
+
+    fun addMovieToList(movie: Movie){
+        movieList.add(movie)
+        movieAdapter.changeList(movieList)
     }
 
     private inner class FetchMovie : AsyncTask <String, Void, String>(){
@@ -41,6 +61,7 @@ class MainActivity : AppCompatActivity() {
 
                 if(movieJson.getString("Response") == "True"){
                     val movie = Gson().fromJson<Movie>(movieInfo, Movie::class.java)
+                    addMovieToList(movie)
                 }else{
                     Snackbar.make(main_ll,"No existe la pelicula en la base",Snackbar.LENGTH_SHORT).show()
                 }
--- app/src/main/java/com/example/laboratorio_04_00195316/adapters/MovieAdapter.kt
+++ app/src/main/java/com/example/laboratorio_04_00195316/adapters/MovieAdapter.kt
@@ -2,24 +2,42 @@ package com.example.laboratorio_04_00195316.adapters
 
 import android.support.v7.view.menu.ActionMenuItemView
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.example.laboratorio_04_00195316.R
 import com.example.laboratorio_04_00195316.model.Movie
+import kotlinx.android.synthetic.main.cardview_movie.view.*
+import java.text.FieldPosition
 
-class MovieAdapter (val moives : List<Movie>) : RecyclerView.Adapter<MovieAdapter.ViewHolder> (){
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-
+class MovieAdapter (var movies : List<Movie>) : RecyclerView.Adapter<MovieAdapter.ViewHolder> (){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.cardview_movie,parent, false)
+        return ViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getItemCount(): Int = movies.size
 
-    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(movies[position])
+
+    fun  changeList(movies: List<Movie>){
+        this.movies= movies
+        notifyDataSetChanged()
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(movie: Movie) = with(itemView){
+            Glide.with(itemView.context)
+                .load(movie.Poster)
+                .placeholder(R.drawable.ic_launcher_background)
+                .into(movie_image_cv)
 
+            movie_title_cv.text = movie.Title
+            movie_plot_cv.text = movie.Plot
+            movie_rate_cv.text = movie.imdbRating
+            movie_runtime_cv.text = movie.Runtime
+
+        }
     }
 }
